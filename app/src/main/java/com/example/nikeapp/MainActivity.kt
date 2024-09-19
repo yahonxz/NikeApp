d9fcb3af8c14125c0fbedd364c3f466feecca5fe
yahonxz <jorge.sandova.c11@gmail.com>
2024-09-18 23:30:45 -0700

    Se hizo el composable para elegir las categorias y el titulo comprar, ademas se integro al contenido principal

--- app/src/main/java/com/example/nikeapp/MainActivity.kt
+++ app/src/main/java/com/example/nikeapp/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Search
@@ -19,12 +25,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.nikeapp.ui.theme.NikeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,18 +42,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SimpleSwitchWithLogos()
+            Column (modifier = Modifier.background(Color.LightGray)){
+                Column (modifier = Modifier.background(Color.White)){
+                    SimpleSwitchWithLogos()
+                    Titulo()
+                }
+                Spacer(modifier = Modifier.height(2.dp))
+            }
+
         }
     }
 }
 
 
-@Preview(showBackground = true)
 @Composable
 fun SimpleSwitchWithLogos() {
     Row(
-        modifier = Modifier.padding(16.dp)
-            .fillMaxWidth(  )
+        modifier = Modifier
+            .padding(16.dp)
+            .statusBarsPadding()
+            .fillMaxWidth()
     ) {
         // Logotipo de Nike
         val nikeLogo: Painter = painterResource(id = R.drawable.nike_logo)
@@ -61,4 +79,43 @@ fun SimpleSwitchWithLogos() {
             contentDescription = "icono de buscar",
             modifier = Modifier.padding(start = 250.dp, bottom = 15.dp))
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun Titulo (){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, top = 6.dp, end = 16.dp)
+    ) {
+        Text(
+            text = "Comprar",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(18.dp),
+            modifier = Modifier.padding(top = 22.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Hombre",
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(bottom = 12.dp))
+                // Línea debajo de "Hombre"
+                Box(
+                    modifier = Modifier
+                        .width(60.dp) // Ajustamos el ancho de la línea al texto
+                        .height(2.dp)
+                        .background(Color.Black)
+                )
+            }
+
+            Text(text = "Mujer")
+            Text(text = "Niño/a")
+        }
+    }
+
 }
